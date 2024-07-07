11aaa7831fe77fa547f67fdbb9b3435b282a3f05
Jamsheed-Khan <jamshedkh365@gmail.com>
2024-07-07 17:33:44 +0500

    working.....

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+// import Signup from "./Components/Signup/Signup.jsx"
+// import Login from "./Components/Login/Login.jsx"
+import Coursescreatemodel from './Components/Coursescreatemodel/Coursescreatemodel.jsx'
+import CoursesModal from './Components/CoursesModal/CoursesModal.jsx'
+import StudentInformationModal from './Components/StudentInformationModal/StudentInformationModal.jsx'
+import TeacherInformationModal from './Components/TeacherInformationModal/TeacherInformationModal.jsx'
+import AdminCoursesPage from './Components/AdminCoursespage/AdminCoursesPage.jsx'
+function App() {
+  return (
+    <>
+     {/* <Coursescreatemodel /> */}
+     {/* <CoursesModal /> */}
+     {/* <StudentInformationModal /> */}
+     {/* <TeacherInformationModal /> */}
+     <AdminCoursesPage />
+    </>
+  )
+}
+
+export default App
